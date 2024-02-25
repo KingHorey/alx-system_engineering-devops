70676f7ef9159a21d41c130d3afe87de65a97617
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-25 22:43:07 +0100

    change from ssh execution to edit config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # connect to server using the connection string
 exec {'ssh_config':
-  command => 'echo -e "\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no" >> /ect/ssh/ssh_config',
+  command => 'echo -e "\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no" >> /etc/ssh/ssh_config',
   path    => ['/usr/bin', '/usr/sbin'],
   returns => [0, 1],
 }
