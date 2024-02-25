e49914959ecc6497666f7c8421e032650f996c20
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-25 21:56:00 +0100

    change PasswordAuthentication

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # connect to server using the connection string
 
 exec { 'ssh':
-  command  => 'ssh -i /home/ubuntu/.ssh/school ubuntu@54.144.139.47',
+  command  => 'ssh -i /home/ubuntu/.ssh/school ubuntu@54.144.139.47 -o PasswordAuthentication=no',
   path     => [ '/usr/bin', '/usr/sbin', '/bin' ],
   provider => 'shell',
 }
