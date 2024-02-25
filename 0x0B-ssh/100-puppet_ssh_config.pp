abc8d4073d48df8b3b4454e92aaa9c21751efd7b
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-25 21:47:54 +0100

    test new manifest config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,7 @@
 # connect to server using the connection string
 
 exec { 'ssh':
-  command => 'ssh -i ~/.ssh/school ubuntu@54.144.139.47 PasswordAuthentication=no',
-  path    => [ '/usr/bin', '/usr/sbin', '/bin' ],
+  command  => 'ssh -i /home/ubuntu/.ssh/school ubuntu@54.144.139.47',
+  path     => [ '/usr/bin', '/usr/sbin', '/bin' ],
+  provider => 'shell',
 }
