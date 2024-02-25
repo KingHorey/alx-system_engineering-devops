f433d6129356c6f797c987c7277bd0e094e9cdd2
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-25 22:27:21 +0100

    new manifest file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,6 @@
 # connect to server using the connection string
-
-# create a file in the server
-exec { 'ssh_school':
-  command  => 'ssh-keygen -b 4096 -f /home/ubuntu/.ssh/school',
-  path     => '/usr/bin:bin',
-  provider => 'shell',
-  returns  => [0, 1],
-}
-
-exec { 'ssh':
-  command  => 'ssh -i /home/ubuntu/.ssh/school ubuntu@54.144.139.47 -o PasswordAuthentication=no',
-  path     => '/usr/bin:bin',
-  provider => 'shell',
-  returns  => [0, 1],
+exec {'ssh_connect':
+  command => '/usr/bin/ssh ubuntu@54.236.44.249 -o IdentityFile=/home/ubuntu/.ssh/school PasswordAuthentication=no',
+  path    => ['/usr/bin', '/usr/sbin'],
+  returns => [0, 1],
 }
