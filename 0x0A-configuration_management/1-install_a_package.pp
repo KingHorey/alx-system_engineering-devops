cd9bd8c2204b01198a898c6afd9a09791677d927
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-25 14:46:38 +0100

    add installation of werkzeug to manifest

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # install flask from pip
 
-package { 'flask':
+package {'flask':
   ensure   => '2.1.0',
+  provider => pip3,
+  require  => Package['werkzeug'],
+}
+
+package {'werkzeug':
+  ensure   => '2.1.1',
   provider => pip3,
 }
