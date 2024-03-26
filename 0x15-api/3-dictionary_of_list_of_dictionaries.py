bc4a561f32b105d24070da6266ecb3ddc54b49c6
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-26 02:30:35 +0100

    create json for all users data

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,24 +10,35 @@ import sys
 def gather_data():
     """ gets mock employee data from an API """
 
-    user_id = sys.argv[1]
-    url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-    data = req.get(url)
-    if data.status_code == 200:
-        length = len(data.json())
-        user_url = "https://jsonplaceholder.typicode.com/users"
-        user_data = req.get(user_url, params=({'id': user_id}))
-        user_data = user_data.json()
-        name = user_data[0].get('username')
+    user_url = f"https://jsonplaceholder.typicode.com/users/?id="
+    todos_url = f"https://jsonplaceholder.typicode.com/todos/"
+    todos = req.get(todos_url)
+    if todos.status_code == 200:
+        todos = todos.json()
+        ids = []
+        for todo in todos:
+            ids.append(todo.get('userId'))
+        ids = list(set(ids))
         tasks_title = []
+        users_dict_data = {}
         """ write to CSV """
-        for data in data.json():
-            tasks_title.append({"username": name,
-                                "task": data.get('title'),
-                                "completed": data.get('completed')
-                                })
+
+        for u_id in ids:
+            name = req.get(user_url, params={'id': user_id})
+            name = name.json()[0].get('username')
+            todo = f"https://jsonplaceholder.typicode.com/users/{u_id}/todos"
+            users_data = req.get(todo, params={'userId': u_id}).json()
+            temp_list = []
+            for data in users_data:
+                temp_list.append({"username": name,
+                                 "task": data.get('title'),
+                                  "completed": data.get('completed')})
+                tasks_title.extend(temp_list)
+                users_dict_data[user_id] = tasks_title
+                tasks_title = []
+
         with open(f"todo_all_employees.json", 'w') as file:
-            file.write(json.dumps({user_id: tasks_title}))
+            file.write(json.dumps(users_dict_data))
 
 
 if __name__ == "__main__":
