d5a41a71d6ac3384b0551d392c807c19bc32008d
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-04-11 13:00:40 +0100

    remove sys module and add multiline comment

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-''' import requests module for making HTTP requests '''
+''' import requests module for making HTTP requests
+    to the REDDIT api
+'''
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
     ''' function that eries the Reddit API and returns the number of
     subscribers '''
-    subrdt = subreddit
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subrdt)
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
