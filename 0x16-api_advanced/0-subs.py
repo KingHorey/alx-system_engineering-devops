ee7c96f4ff0509f86e6e5c00ba322960c1680ca2
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-04-14 21:33:55 +0100

    try new

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-
-''' import requests module for making HTTP requests
-    to the REDDIT api
-'''
-import requests as req
+"""
+ a function that queries the Reddit API and returns
+ the number of subscribers (not active users,
+ total subscribers) for a given subreddit.
+ If an invalid subreddit is given, the function should return 0.
+ """
+import requests
 
 
 def number_of_subscribers(subreddit):
-    ''' function that eries the Reddit API and returns the number of
-    subscribers '''
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = req.get(url, headers=headers, allow_redirects=False)
+    """
+    returns numbers of subscriber
+    if subreddit not valid returns 0
+    """
+    headers = {"User-Agent": "Scrribbles"}
+    url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=True)
     if response.status_code != 200:
         return 0
-    else:
-        data = response.json().get('data').get('subscribers')
-        if data is None:
-            return 0
-        return data
+    users = response.json()
+    if users.get("data").get('subscribers'):
+        return users.get("data").get('subscribers')
+    return 0
