4668d7ed8af18a0bbfdb3500eac1d2d198386964
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-04-17 12:21:04 +0100

    fix strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,7 @@
-# strace revealed that a file with extention phpp does not exist
-# that is an error in some cconfiguration we need to correcct here
+# strace did not provide revealing errors, even when run with -e flag
+# defautled to asking
 
 exec { 'change wp-locale extension':
     command  => 'sed  -i  s/phpp/php/g /var/www/html/wp-settings.php',
     provider => shell,
-# after a rigorous search, trace revealed that a file with extention phpp does not exist
-# that is an error in some cconfiguration we need to correcct here
-
-exec { 'change wp-locale extension':
-    command  => 'sed  -i  s/phpp/php/g /var/www/html/wp-settings.php',
-    provider => shell,
-}}
+}
